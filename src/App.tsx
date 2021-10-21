9d710e3073f3cb67c3d18f97a619bbad27405fe5
Dmitriy Fishman <fishman.code@gmail.com>
2021-10-21 17:06:26 +0300

    Added a sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
+import { useFocusTrap, useMergedRef } from '@mantine/hooks';
 import './App.css';
 
 function App() {
+  const focusTrapRef = useFocusTrap();
+  const [value, setValue] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //<div ref={focusTrapRef}>
+    <div ref={useMergedRef(focusTrapRef)}>
+      <input value={value} type="text" onChange={(e) => { setValue(e.target.value) }} />
+      <input type="text" data-autofocus />
     </div>
   );
 }
